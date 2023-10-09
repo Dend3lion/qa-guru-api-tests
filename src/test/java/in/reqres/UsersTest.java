437bf851b1ee569c231868dc6b2638a3da4b24db
Denis Bashkatov <dend3lion@gmail.com>
2023-10-09 20:53:29 +0300

    fix: formatted file

--- src/test/java/in/reqres/UsersTest.java
+++ src/test/java/in/reqres/UsersTest.java
@@ -4,12 +4,11 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.Matchers.is;
-
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
-public class usersTest {
+public class UsersTest {
     @BeforeAll
     static void beforeAll() {
         RestAssured.baseURI = "https://reqres.in";
@@ -21,35 +20,37 @@ public class usersTest {
         String requestBody = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
 
         given()
-            .log().uri()
-            .log().method()
-            .log().body()
-            .contentType(ContentType.JSON)
-            .body(requestBody)
-        .when()
-            .post("/users")
-        .then()
-            .log().status()
-            .log().body()
-            .statusCode(201)
-            .body("name", is("morpheus"))
-            .body("job", is("leader"));
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
     }
+
     @Test
     void validListAllUsers() {
         given()
-            .log().uri()
-            .log().method()
-        .when()
-            .get("/users")
-        .then()
-            .log().status()
-            .log().body()
-            .statusCode(200)
-            .body("page", is(1))
-            .body("total", is(12))
-            .body("data[0].id", is(1));
+                .log().uri()
+                .log().method()
+                .when()
+                .get("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("page", is(1))
+                .body("total", is(12))
+                .body("data[0].id", is(1));
     }
+
     @Test
     void validListPageUsers() {
         given()
@@ -71,22 +72,23 @@ public class usersTest {
         given()
                 .log().uri()
                 .log().method()
-            .when()
+                .when()
                 .get("/users/3")
-            .then()
+                .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
                 .body("data.id", is(3));
     }
+
     @Test
     void invalidShowUser() {
         given()
                 .log().uri()
                 .log().method()
-            .when()
+                .when()
                 .get("/users/100500")
-            .then()
+                .then()
                 .log().status()
                 .log().body()
                 .statusCode(404);
